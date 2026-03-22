70c8ffd0fd2e4a743901dd621f335e3cb71d1c10
ayushsingh2610-k <sweetayushsingh7538@gmail.com>
2026-03-22 16:16:42 +0530

    Added Bottom-Up Level Order Traversal (BFS)

---
+++ Binary-Trees/01_Traversals/LevelOrderBottom.cpp
@@ -0,0 +1,25 @@
+//Using BFS + Reverse
+// Time: O(n) | Space: O(n)
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+           queue<TreeNode* >q;
+    if(root==NULL)return {};
+    q.push(root);
+    vector<vector<int>>ans;
+    while(q.size()!=0){
+        int s=q.size();
+        vector<int>v;
+        for(int i =0;i<s;i++){
+        TreeNode* a=q.front();
+        q.pop();
+         if(a!=NULL)v.push_back(a->val);
+         if(a->left!=NULL)q.push(a->left);
+         if(a->right!=NULL)q.push(a->right);
+        }
+        ans.push_back(v);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+    }
+};
